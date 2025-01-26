6cede0f156ff02f32feb4bebe1660c920ab352bd
Kentaro1043 <71170923+Kentaro1043@users.noreply.github.com>
2025-01-26 21:43:44 +0900

    🎨 シェルスクリプトに変更

---
+++ vaultwarden-backup/config.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Ref: https://github.com/ttionya/vaultwarden-backup#configure-rclone-%EF%B8%8F-must-read-%EF%B8%8F
+docker run --rm -it \
+  --mount type=volume,source=vaultwarden-backup_rclone,target=/config/ \
+  ttionya/vaultwarden-backup:latest \
+  rclone config
